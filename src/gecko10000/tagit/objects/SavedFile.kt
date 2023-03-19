2f6db90bd80aa246b658c7faa554ca8832bbdc1e
gecko10000 <levtey@gmail.com>
2023-03-19 00:29:42 -0700

    Use thread-safe sets

--- src/gecko10000/tagit/objects/SavedFile.kt
+++ src/gecko10000/tagit/objects/SavedFile.kt
@@ -1,13 +1,16 @@
 package gecko10000.tagit.objects
 
 import gecko10000.tagit.serializers.FileSerializer
+import io.netty.util.internal.ConcurrentSet
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 import java.io.File
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentSkipListSet
 
 @Serializable
 data class SavedFile(@Serializable(with = FileSerializer::class)
                      val file: File) {
     @Transient
-    val tags: MutableSet<Tag> = mutableSetOf()
+    val tags: MutableSet<Tag> = ConcurrentSkipListSet()
 }
--- src/gecko10000/tagit/objects/Tag.kt
+++ src/gecko10000/tagit/objects/Tag.kt
@@ -2,12 +2,13 @@ package gecko10000.tagit.objects
 
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
+import java.util.concurrent.ConcurrentSkipListSet
 
 @Serializable
 data class Tag(val name: String, val parent: Tag? = null) {
     @Transient
-    val subtags: Set<Tag> = mutableSetOf()
+    val subtags: MutableSet<Tag> = ConcurrentSkipListSet()
 
     @Transient
-    val files: Set<SavedFile> = mutableSetOf()
+    val files: MutableSet<SavedFile> = ConcurrentSkipListSet()
 }
