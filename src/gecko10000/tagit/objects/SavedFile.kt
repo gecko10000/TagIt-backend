3a9f90034d6a1a57bffb82cb946496daebab604d
gecko10000 <levtey@gmail.com>
2023-04-30 00:49:34 -0700

    Return MIME types with SavedFiles

--- src/gecko10000/tagit/objects/SavedFile.kt
+++ src/gecko10000/tagit/objects/SavedFile.kt
@@ -2,6 +2,7 @@ package gecko10000.tagit.objects
 
 import gecko10000.tagit.serializers.FileSerializer
 import gecko10000.tagit.serializers.TagStringSerializer
+import io.ktor.server.http.content.*
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import java.io.File
@@ -12,12 +13,13 @@ data class SavedFile(
     @SerialName("name")
     @Serializable(with = FileSerializer::class)
     val file: File,
-
+    val mimeType: String,
     val tags: MutableSet<@Serializable(with = TagStringSerializer::class) Tag> = ConcurrentSkipListSet(compareBy { it.name })
-    ) : Nameable() {
-    /*fun addTags(vararg newTags: Tag) = SavedFile(file, buildSet(tags.size + newTags.size) {
-        addAll(tags)
-        addAll(newTags.map{ it.name })
-    })*/
+) : Nameable() {
+    constructor(file: File) : this(
+        // withoutParameters() removes, for example, `; charset=UTF-8` from `text/plain; charset=UTF-8`
+        file, LocalFileContent(file).contentType.withoutParameters().toString()
+    )
+
     override fun name(): String = file.name
 }
