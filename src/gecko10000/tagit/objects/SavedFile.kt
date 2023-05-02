0883a0f2598f6e75bf141a805f86a01ea7d9f5ca
gecko10000 <levtey@gmail.com>
2023-05-02 12:19:23 -0700

    Change SavedFile encoding

--- src/gecko10000/tagit/objects/SavedFile.kt
+++ src/gecko10000/tagit/objects/SavedFile.kt
@@ -2,24 +2,16 @@ package gecko10000.tagit.objects
 
 import gecko10000.tagit.serializers.FileSerializer
 import gecko10000.tagit.serializers.TagStringSerializer
-import io.ktor.server.http.content.*
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import java.io.File
 import java.util.concurrent.ConcurrentSkipListSet
 
 @Serializable
 data class SavedFile(
-    @SerialName("name")
     @Serializable(with = FileSerializer::class)
     val file: File,
-    val mimeType: String,
     val tags: MutableSet<@Serializable(with = TagStringSerializer::class) Tag> = ConcurrentSkipListSet(compareBy { it.name })
 ) : Nameable() {
-    constructor(file: File) : this(
-        // withoutParameters() removes, for example, `; charset=UTF-8` from `text/plain; charset=UTF-8`
-        file, LocalFileContent(file).contentType.withoutParameters().toString()
-    )
 
     override fun name(): String = file.name
 }
--- src/gecko10000/tagit/serializers/FileSerializer.kt
+++ src/gecko10000/tagit/serializers/FileSerializer.kt
@@ -1,17 +1,28 @@
 package gecko10000.tagit.serializers
 
 import gecko10000.tagit.misc.fileDirectory
+import io.ktor.server.http.content.*
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.descriptors.element
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.io.File
 
 class FileSerializer : KSerializer<File> {
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("File", PrimitiveKind.STRING)
+    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("File") {
+        element<String>("name")
+        element<String>("mimeType")
+        element<Long>("modificationDate")
+    }
 
-    override fun serialize(encoder: Encoder, value: File) = encoder.encodeString(value.name)
+    override fun serialize(encoder: Encoder, value: File) {
+        val structure = encoder.beginStructure(descriptor)
+        structure.encodeStringElement(descriptor, 0, value.name)
+        structure.encodeStringElement(descriptor, 1, LocalFileContent(value).contentType.withoutParameters().toString())
+        structure.encodeLongElement(descriptor, 2, value.lastModified())
+        structure.endStructure(descriptor)
+    }
     override fun deserialize(decoder: Decoder) = File(fileDirectory + decoder.decodeString())
 }
