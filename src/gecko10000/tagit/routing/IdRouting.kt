78ec6ea3b2887d92d2d1781c28c76a37151e4de9
gecko10000 <levtey@gmail.com>
2023-05-27 17:19:15 -0700

    Improve ID endpoint

--- src/gecko10000/tagit/routing/IdRouting.kt
+++ src/gecko10000/tagit/routing/IdRouting.kt
@@ -1,14 +1,14 @@
 package gecko10000.tagit.routing
 
-import io.ktor.http.HttpStatusCode.Companion.OK
+import gecko10000.tagit.misc.VERSION
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Routing.idRouting() {
-    route("/is/this/a/tagit/backend") {
-        get {
-            call.respond(OK, "It sure is.")
+    route("/tagit") {
+        get("version") {
+            call.respond("TagIt Backend Version: $VERSION")
         }
     }
 }
