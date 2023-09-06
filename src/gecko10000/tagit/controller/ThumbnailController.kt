7139973d0768d331eaebe957d53d51036b9d8ed4
gecko10000 <levtey@gmail.com>
2023-09-06 12:27:24 -0700

    Improve video thumbnail process

--- src/gecko10000/tagit/controller/ThumbnailController.kt
+++ src/gecko10000/tagit/controller/ThumbnailController.kt
@@ -25,30 +25,43 @@ class ThumbnailController(files: SavedFileMap) {
     }
 
     private fun getOutputFile(savedFile: SavedFile): File {
-        val outputFileName = "${savedFile.file.nameWithoutExtension}.$thumbnailFormat"
+        // note: we must use the full filename.
+        // Without the extension, we may get collisions
+        // like files screenshot.png and screenshot.jpg
+        // having the same thumbnail filename.
+        val outputFileName = "${savedFile.file.name}.$thumbnailFormat"
         return dataDirectory.thumbnail.resolve(outputFileName)
+    }
+
+    private fun convertThumb(input: File, output: File) {
+        Thumbnails.of(input)
+            .size(thumbnailSize, thumbnailSize)
+            .crop(Positions.CENTER)
+            .toFile(output)
     }
 
     private fun makeImageThumb(savedFile: SavedFile): File {
         val outputFile = getOutputFile(savedFile)
-        Thumbnails.of(savedFile.file)
-            .size(thumbnailSize, thumbnailSize)
-            .crop(Positions.CENTER)
-            .toFile(outputFile)
+        convertThumb(savedFile.file, outputFile)
         return outputFile
     }
 
     private fun makeVideoThumb(savedFile: SavedFile): File {
         val file = savedFile.file
-        val outputFile = getOutputFile(savedFile)
+        // note: we use .<extension> for the suffix so ffmpeg recognizes the filetype
+        val tempFile = File.createTempFile("tagit", ".$thumbnailFormat");
         val builder = FFmpegBuilder()
             .addExtraArgs("-discard", "nokey")
             .addInput(file.path)
-            .setVideoFilter("thumbnail,scale=$thumbnailSize:$thumbnailSize")
-            .addOutput(outputFile.path)
+            .setVideoFilter("thumbnail")
+            .addOutput(tempFile.path)
             .setFrames(1)
             .done()
         ffmpeg.run(builder)
+
+        val outputFile = getOutputFile(savedFile)
+        convertThumb(tempFile, outputFile)
+        tempFile.delete()
         return outputFile
     }
 
