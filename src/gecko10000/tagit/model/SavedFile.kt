36a2c82baa7b0c8cb5d571ed3fdc428373d3e061
gecko10000 <levtey@gmail.com>
2023-10-31 19:21:28 -0700

    Use guessContentTypeFromStream

--- src/gecko10000/tagit/model/SavedFile.kt
+++ src/gecko10000/tagit/model/SavedFile.kt
@@ -3,8 +3,10 @@ package gecko10000.tagit.model
 import gecko10000.tagit.misc.extension.getUUID
 import gecko10000.tagit.model.enum.MediaType
 import gecko10000.tagit.model.mapper.ModelMapper
+import java.io.BufferedInputStream
 import java.io.File
-import java.nio.file.Files
+import java.io.FileInputStream
+import java.net.URLConnection
 import java.util.*
 
 data class SavedFile(
@@ -13,9 +15,13 @@ data class SavedFile(
 ) {
     val uuid: UUID = file.getUUID()
 
-    // probeContentType is better than using LocalFileContent
-    // because LFC just looks at the extension
-    val mimeType: String? = Files.probeContentType(file.toPath())
+    // don't want to just check the extension
+    val mimeType: String? = run {
+        val inputStream = BufferedInputStream(FileInputStream(file))
+        URLConnection.guessContentTypeFromStream(inputStream)
+    }
+
+    //val mimeType: String? = Files.probeContentType(file.toPath())
     val mediaType = run {
         mimeType?.let { ModelMapper.MEDIA_TYPE.apply(it) } ?: MediaType.UNKNOWN
     }
